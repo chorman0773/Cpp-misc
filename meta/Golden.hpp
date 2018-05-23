c94684d1aef8b2298b71674912e99395a1e335a7
InfernoDeity <chorman64@gmail.com>
2018-05-23 08:30:20 -0400

    Update Golden.hpp

--- meta/Golden.hpp
+++ meta/Golden.hpp
@@ -1,6 +1,21 @@
 #ifndef __Golden_hpp_2018_05_23_08_17
 #define __Golden_hpp_2018_05_23_08_17
 
+#include <ratio>
+#include <cstddef>
+
+using one = std::ratio<1,1>;
+
+template<size_t max,typename prev=std::ratio<1,1>> struct golden{
+  using value = std::ratio_add<one,std::ratio_divide<one,prev>>;
+  using next = golden<max-1,value>;
+  constexpr static double φ = value::num/(double)value::denom;
+};
+template<typename prev> struct golden<0,prev>{
+  using value = std::ratio_add<one,std::ratio_divide<one,prev>>;
+  constexpr static double φ = value::num/(double)value::denom;
+};
+
 
 
 
